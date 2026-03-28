6b614c8594e134dee48a229f1eb6c5bcbf3c630f
Xanion11 <krzysztof.j.ciesla@gmail.com>
2026-03-28 23:15:08 +0100

    not working drawing function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <cstdint>
+#include <cstring>
 #include <iostream>
+#include <assert.h>
+#include <iterator>
 #include <stdint.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 #include <vector>
 #include "font.h"
 #include <bitset>
@@ -30,10 +35,27 @@ void ParsingBrightness(unsigned char *input, std::vector<int> *vector, int image
 
 }
 
+void Write_File(std::vector<int> *vec, int width, int height, unsigned char *ret){
+    // repeat for x pixels y times
+    // for each pixel write 8x8 square from font.h depending on the value
+    // quadruple nested for loop (god help me)
+    //TODO make run fast
+    //
+    //
+    //
+    //
+    // memset(ret, 0x00, width*height*8*8);
+    int i = 0;
+    while (i < width*height*8*8) {
+        ret[i] = 0xff;
+    }
+
+
+}
+
 
 int main() {
     int x, y, n = 0;
-    std::vector<float> image_vector;
 
     unsigned char *data = stbi_load("test.png",&x,&y,&n,1);
     if (data == nullptr) {
@@ -42,6 +64,17 @@ int main() {
     std::vector<int> character_strip;
 
     ParsingBrightness(data, &character_strip, x, y);
+    stbi_image_free(data);
+
+    const size_t arrsize = 8*y*x*8;
+
+    unsigned char *image_data = new unsigned char[arrsize];
+
+    Write_File(&character_strip, x, y, image_data);
+
+    stbi_write_jpg("output.jpg", 8*x, 8*y, 1, image_data, 100);
+
+    delete[] image_data;
 
 
     return 0;
